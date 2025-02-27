a79c2780140c31aaab0469820c9d3e70f1fff85e
HenryPresidente <Henry@codersfree.com>
2025-02-27 17:26:35 -0600

    Agregar archivos de proyecto.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace P01_2022_PL_601_2023_RR_655
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
