f411080b5a802ecd4c9a029f336ebcaaa4f53a83
Diegorm97 <diego.ramirez3@catolica.edu.sv>
2025-02-27 18:18:38 -0600

    clases del models

---
+++ Models/Reserva.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P01_2022_PL_601_2023_RR_655.Models
+{
+    public class Reserva
+    {
+        [Key]
+
+            public int Id { get; set; }
+            public int UsuarioId { get; set; }
+            public Usuario Usuario { get; set; }
+            public int EspacioId { get; set; }
+            public EspacioDeParqueo Espacio { get; set; }
+            public DateTime FechaReserva { get; set; }
+            public int HorasReservadas { get; set; }
+            public bool Activa { get; set; } 
+        
+
+    }
+}
